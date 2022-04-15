087299f9f99d6c723263dfd7852b8be5a354816a
oniriik <oniriik.dev@gmail.com>
2022-04-15 16:11:25 +0200

    update

--- server copy.js
+++ server copy.js
@@ -1,17 +1,12 @@
-// Imports
+// Express setup
 const express = require('express');
 const axios = require('axios');
-// Express setup
 const res = require('express/lib/response');
-const { redirect } = require('express/lib/response');
 const app = express();
 app.set('view engine', 'ejs');
-
-app.listen(process.env.PORT ||8080, () => {
-    console.log(`\n\n\n\n\n/// RUNNING ///\n`)
-  })
-
+app.listen(process.env.PORT ||8080);
 app.use('/static', express.static('public'));
+
 
 app.get('/redirect', (req, res) => {
     messages = [
@@ -22,23 +17,17 @@ app.get('/redirect', (req, res) => {
     ]
     path = locations[req.query.id]
     msg = messages[req.query.id]
+    console.log(path,msg)
     res.render('redirect',{msg,path})
 });
 
 
-function checkIfInDB(toCheck,moviesList){
-    for(const element of moviesList){
-        if(element.imdbID == toCheck){
-            console.log(element.title , " exist")
-            return true
-        }
-    }
-    return false
-}
 
-app.get('/search', (req, res) => {
-    moviesList = []
-    axios.get('https://allocinoche.herokuapp.com/api/movies?sort=id&oder=asc').then(resp => {
+
+// Fucntions
+const fetchMovies = async(res) => {
+    let movies = []
+    await axios.get('https://allocinoche.herokuapp.com/api/movies?sort=id&oder=asc').then(resp => {
             movieQuery = resp['data'].data
             // Format movieQuery for easier usage
             for (let i = 0; i < movieQuery.length; i++) {
@@ -53,42 +42,17 @@ app.get('/search', (req, res) => {
                     'actors': movieQuery[i].attributes.actors,
                     'imdbID' : movieQuery[i].attributes.imdbID
                 }
-                moviesList.push(currentMovie)
-            }
-            const movieToSearch = req.query.movie;
-            const options = {
-                method: 'GET',
-                url: 'https://movie-database-alternative.p.rapidapi.com/',
-                params: {s: movieToSearch, r: 'json', page: '1'},
-                headers: {
-                  'X-RapidAPI-Host': 'movie-database-alternative.p.rapidapi.com',
-                  'X-RapidAPI-Key': '2fba6ce8f4msh955533e104418b4p14244bjsn30c181fb15da'
-                }
-              };
-              
-              axios.request(options).then(function (response) {
-                  let foundMovies = []
-                  searchQuery = response.data.Search;
-                  // Format searchQuery for easier usage
-                  for (let i = 0; i < searchQuery.length; i++) {
-                      const currentMovie = {
-                          'title': searchQuery[i].Title,
-                          'release': searchQuery[i].Year,
-                          'cover_path': searchQuery[i].Poster,
-                          'imdbID': searchQuery[i].imdbID,
-                          'alreadyInDB' : checkIfInDB(searchQuery[i].imdbID ,moviesList)
-                      }
-                      foundMovies.push(currentMovie)
-                  }
-                  console.log(foundMovies[1])
-                  res.render('addmovie',{foundMovies});
-              }).catch(function (error) {
-                  console.error(error);
-              });
+                movies.push(currentMovie)
+            } 
+        }).then(()=>{
+            res.render('mainpage',{movies})
         });
-});
+
+}
 
 async function addToBDD(toSend){
+    await axios.get('https://allocinoche.herokuapp.com/api/movies?filters[imdbID][$eq]='+toSend.imdbID).then(resp => {
+    if(resp['data'].data.length == 0){
         let rating  = Math.round(parseInt(toSend.imdbRating)/2)
         axios.post('https://allocinoche.herokuapp.com/api/movies',{
             "data":{
@@ -108,7 +72,27 @@ async function addToBDD(toSend){
           .catch(error=>{
             console.log(error)
           });
+        }else{
+            console.log("already in db")    
+        }
+        
+    });
     }
+    
+    
+function checkIfInDB(toCheck){
+    console.log(global.movies)
+
+    for(const element of movies){
+        console.log(element.imdbID)
+        console.log(element)
+        if(element.imdbID == toCheck){
+            console.log(element + " exist")
+            return true
+        }
+    }
+    return false
+}
 
 app.get('/addmovie',async(req, res) => {
     const toAdd = req.query.toAdd;
@@ -149,26 +133,47 @@ app.get('/addmovie',async(req, res) => {
         res.redirect('/redirect?id=0')
 });
 
-app.get('*',(req, res) => {
-    let movies = []
-    axios.get('https://allocinoche.herokuapp.com/api/movies?sort=id&oder=asc').then(resp => {
-            movieQuery = resp['data'].data
-            // Format movieQuery for easier usage
-            for (let i = 0; i < movieQuery.length; i++) {
-                const currentMovie = {
-                    'title': movieQuery[i].attributes.title,
-                    'plot': movieQuery[i].attributes.plot,
-                    'release': movieQuery[i].attributes.release,
-                    'rating': movieQuery[i].attributes.rating,
-                    'cover_path': movieQuery[i].attributes.cover_path,
-                    'director': movieQuery[i].attributes.director,
-                    'writter': movieQuery[i].attributes.writter,
-                    'actors': movieQuery[i].attributes.actors,
-                    'imdbID' : movieQuery[i].attributes.imdbID
-                }
-                movies.push(currentMovie)
-            }
-            res.render("mainpage",{movies})
-        });
 
+app.get('/search', (req, res) => {
+    const movieToSearch = req.query.movie;
+    
+    const options = {
+        method: 'GET',
+        url: 'https://movie-database-alternative.p.rapidapi.com/',
+        params: {s: movieToSearch, r: 'json', page: '1'},
+        headers: {
+          'X-RapidAPI-Host': 'movie-database-alternative.p.rapidapi.com',
+          'X-RapidAPI-Key': '2fba6ce8f4msh955533e104418b4p14244bjsn30c181fb15da'
+        }
+      };
+      
+      axios.request(options).then(function (response) {
+          let foundMovies = []
+          searchQuery = response.data.Search;
+          // Format searchQuery for easier usage
+          for (let i = 0; i < searchQuery.length; i++) {
+              const currentMovie = {
+                  'title': searchQuery[i].Title,
+                  'release': searchQuery[i].Year,
+                  'cover_path': searchQuery[i].Poster,
+                  'imdbID': searchQuery[i].imdbID,
+                  'alreadyInDB' : checkIfInDB(searchQuery[i].imdbID)
+              }
+              console.log(currentMovie)
+              foundMovies.push(currentMovie)
+          }
+          res.render('addmovie',{foundMovies});
+      }).catch(function (error) {
+          console.error(error);
+      });
+    
+});
+
+
+
+
+
+
+app.get('/',async(req, res) => {
+        fetchMovies(res)
 });
